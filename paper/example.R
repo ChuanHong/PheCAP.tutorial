e447f6f13d7bea77c98cfc3a5459b1eafbbae103
Chuan <hong.chuan.hannah@gmail.com>
2021-06-21 15:02:53 -0400

    update

---
+++ paper/example.R
@@ -0,0 +1,2 @@
+remotes::install_github("https://github.com/ChuanHong/PheCAP.tutorial", ref="testing")
+devtools::install_github("https://github.com/covidclinical/Phase2.1SurvivalRPackage", subdir="FourCePhase2.1Survival", upgrade=FALSE)
